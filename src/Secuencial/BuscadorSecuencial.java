a36dbbaa7f287203ee7186e956f80a2d618400b2
hiken13 <mancraft13@hotmail.es>
2015-10-16 02:10:41 -0600

    Buscar en varias URLs, desde archivo

--- src/Secuencial/BuscadorSecuencial.java
+++ src/Secuencial/BuscadorSecuencial.java
@@ -5,8 +5,10 @@
  */
 package Secuencial;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -25,7 +27,7 @@ public class BuscadorSecuencial {
     public String leerPagina(String pagina) {
         try {
             Document doc = Jsoup.connect(pagina).get();
-            String title = doc.body().text();
+            String title = doc.text();
             return title;
         } catch (IOException e) {
             System.out.println("Error leyendo el contenido de la página");
@@ -49,25 +51,112 @@ public class BuscadorSecuencial {
         return palabras;
     }
 
-    public int buscarCoincidencia(String palabra, String[] palabras) {
+    private String capitalize(String palabra) {
+        return Character.toUpperCase(palabra.charAt(0)) + palabra.substring(1);
+    }
+
+    /*public int buscarCoincidencia(String palabra, String[] palabras) {
+     int coincidencias = 0;
+     String palabraCap = capitalize(palabra);
+
+     for (int i = 0; i < palabras.length; i++) {
+     if (palabras[i].equals(palabra) || palabras[i].equals(palabra + ",")
+     || palabras[i].equals(palabra + ".") || palabras[i].equals(palabra + ":")
+     || palabras[i].equals(palabra + ";") || palabraCap.equals(palabras[i])
+     || palabras[i].equals("("+palabra+")") || palabras[i].equals("("+palabra+").")
+     || palabras[i].equals("("+palabra+"),")|| palabras[i].equals(palabra+")")
+     || palabras[i].equals("("+palabra)
+     || palabraCap.equals(palabras[i]+",") || palabraCap.equals(palabras[i]+".")
+     || palabraCap.equals(palabras[i]+":") || palabraCap.equals(palabras[i]+";")
+     || palabraCap.equals("("+palabras[i]+")") || palabraCap.equals("("+palabras[i]+").")
+     || palabraCap.equals("("+palabras[i]+"),")|| palabraCap.equals(palabras[i]+")")
+     || palabraCap.equals("("+palabras[i])) {
+     System.out.println(palabras[i]);
+     coincidencias++;
+     }
+     }
+     return coincidencias;
+     }*/
+    /**
+     * Función para buscar las coincidencias de una palabra en el contenido de
+     * la página
+     *
+     * @param palabra la palabra a buscar en el contenido
+     * @param contenido el texto de la página en el que se buscan coincidencias
+     * @return la cantidad de coincidencias encontradas
+     */
+    public int buscarCoincidencia(String palabra, String contenido) {
         int coincidencias = 0;
-        for (int i = 0; i < palabras.length; i++) {
-            if(palabras[i]==palabra){
-                coincidencias ++;
+        while (contenido.contains(palabra)) {
+            contenido = contenido.substring(contenido.indexOf(palabra) + palabra.length(), contenido.length());
+            coincidencias++;
+        }
+        return coincidencias;
+    }
+
+    /**
+     * Función para leer los url de las páginas del archivo y almacenarlos en un
+     * arreglo, para mantenerlos en memoria dinámica
+     *
+     * @param rutaArchivo la ruta del archivo a leer
+     * @return un arreglo con los urls leídos
+     */
+    public String[] leerURLs(String rutaArchivo) {
+        File archivo;
+        FileReader fr = null;
+        BufferedReader br;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(rutaArchivo);
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            int largo = 0;//cantidad de urls
+            while (br.readLine() != null) {
+                largo++;
+            }
+            // Lectura del fichero
+            String linea;
+            String[] urls = new String[largo];
+
+            fr = new FileReader(archivo);//resetear valores del fileReader
+            br = new BufferedReader(fr);//resetear valores del bufferReader
+
+            int i = 0;//indice para el arreglo        
+            while (i < largo) {
+                linea = br.readLine();
+                urls[i] = linea;
+                i++;
+            }
+            return urls; // arreglo con las URLS
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
             }
         }
-        
-        return coincidencias;
+        return null;
     }
 
     public static void main(String[] args) {
         BuscadorSecuencial busqueda = new BuscadorSecuencial();
-
-        String contenido = busqueda.leerPagina("https://es.wikipedia.org/wiki/Vida");
-        System.out.println("/////////////////////////////////////");
-        //System.out.println(contenido);
-        System.out.println("largo de contenido: " + contenido.length());
-        System.out.println("/////////////////////////////////////");
-        busqueda.split(contenido);
+        String[] urls = busqueda.leerURLs("//home//manfred//NetBeansProjects//TaskforceProjects//Buscador//trunk//src//Secuencial//urls.txt");
+        if (urls != null) {
+            for (int i = 0; i < urls.length; i++) {
+                String contenido = busqueda.leerPagina(urls[i]);
+                int coincidencias = busqueda.buscarCoincidencia("Emprego", contenido);
+                System.out.println("Coincidencias en " + urls[i] + " : " + coincidencias);
+            }
+        }
     }
 }
